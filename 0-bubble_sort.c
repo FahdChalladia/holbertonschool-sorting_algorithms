e5d8ba300a3ce82d6b6538c084bf801c9912c295
fahdchalladia <fahdchalladia2004@gmail.com>
2025-04-16 21:59:18 +0100

    task1 files + updated header

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stddef.h>
+/**
+ * swap - swap two integres
+ * @arr: pointer to the array
+ * @i: 1st case adress
+ * @j: sec case adress
+ */
+void swap(int *arr, int i, int j)
+{
+int temp;
+temp = arr[i];
+arr[i] = arr[j];
+arr[j] = temp;
+}
+
+/**
+ * bubble_sort - sort an array using bubble sort algo
+ * @array: pointer to the array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i, j;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+print_array(array, size);
+if (array[j] > array[j + 1])
+swap(array, j, j + 1);
+}
+}
+}
