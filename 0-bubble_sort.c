5eb61da07893a1fd2ab79992b88b5bcb2aea591f
fahdchalladia <fahdchalladia2004@gmail.com>
2025-04-16 22:02:37 +0100

    print after swapping

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,9 +26,9 @@ for (i = 0; i < size - 1; i++)
 {
 for (j = 0; j < size - i - 1; j++)
 {
-print_array(array, size);
 if (array[j] > array[j + 1])
 swap(array, j, j + 1);
+print_array(array, size);
 }
 }
 }
